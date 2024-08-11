fc7c2e5571e08b5d295efd8ccd43fd410584a7f1
harshnimbhekar13 <harshnimbhekar890@gmail.com>
2024-08-11 17:45:29 +0530

    calculator with using two numbers

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a = 12
+b = 2
+addition = a + b
+print("Addition of ",a,"and",b,"is",addition)
+subtraction = a - b
+print("Subtraction of ",a,"and",b,"is",subtraction)
+multiplication = a * b
+print("Multiplication of ",a,"and",b,"is",multiplication)
+division = a / b
+print("Division of ",a,"and",b,"is",division)
+module = a % b 
+print("Module of ",a,"and",b,"is", module)
+
