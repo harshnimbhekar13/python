14ddc6ba2da71aa36990c5a3314b8b32dda92018
harshnimbhekar13 <harshnimbhekar890@gmail.com>
2024-08-22 11:06:18 +0530

    Reverse a number

---
+++ reversenumber.py
@@ -0,0 +1,7 @@
+number_string = input("Enter the number : ")
+
+number_string_reversed = number_string[::-1]
+number_reversed = int (number_string_reversed)
+number_reversed = number_reversed + 10
+
+print(number_reversed)
