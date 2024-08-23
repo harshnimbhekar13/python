0a0770b03f8273b8150ef63cf4d518a6cce131ba
harshnimbhekar13 <harshnimbhekar890@gmail.com>
2024-08-23 22:04:18 +0530

    Find Sum And Average

---
+++ sumandaverage.py
@@ -0,0 +1,17 @@
+sum = 0.0
+nocount = 0
+
+while True:
+    number = float(input("Enter the number : "))
+    sum += number
+    nocount += 1
+    
+    choice = input("Add another number ?  ( y/n ) : " )
+    if choice.casefold() =='n':
+      break
+    
+    average = sum / nocount 
+    
+    print(f"sum : {sum}")
+    print(f"average : {average}")
+    
